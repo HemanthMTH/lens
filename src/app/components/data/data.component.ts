e4eeab4b26f482d9149d808d6e309ae8489c05e9
Hemanth Reddy <hemanthreddysamidi3@email.com>
2023-06-10 16:58:53 -0400

    added a grid

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -6,7 +6,7 @@ import {
     GridReadyEvent,
     ICellRendererParams,
 } from 'ag-grid-community';
-import { ProductData } from 'src/app/models/data';
+import { Feature, Keyword, ProductData } from 'src/app/models/data';
 // import _data from '../../../assets/explore/combined.json';
 import featureData from '../../../assets/explore/features.json';
 import keywordData from '../../../assets/explore/keywords.json';
@@ -25,7 +25,8 @@ import { UrlRendererComponent } from '../url-renderer/url-renderer.component';
 })
 export class DataComponent {
     @ViewChild('featuresModal') featuresModal: FeatureModalComponent;
-
+    selected: ProductData[] = [];
+    showFeatures: boolean;
     columnDefs: ColDef[] = [
         {
             headerName: 'Keywords',
@@ -74,8 +75,94 @@ export class DataComponent {
             },
         },
     ];
+    public keywordColumnDefs: ColDef[] = [
+        {
+            headerName: 'Manufacturer URL',
+            field: 'url',
+            pinned: 'left',
+        },
+        {
+            headerName: 'Do Not Track',
+            field: 'do_not_track',
+        },
+        {
+            headerName: 'Data Security',
+            field: 'data_security',
+        },
+        {
+            headerName: 'First Party Collection',
+            field: 'first_party_collection',
+        },
+        {
+            headerName: 'Third Party Collection',
+            field: 'third_party_collection',
+        },
+        {
+            headerName: 'OPT-OUT',
+            field: 'opt_out',
+        },
+        {
+            headerName: 'User Choice',
+            field: 'user_choice',
+        },
+        {
+            headerName: 'Data',
+            field: 'data',
+        },
+        {
+            headerName: 'Legislation',
+            field: 'legislation',
+        },
+        {
+            headerName: 'Access/Edit/Delete',
+            field: 'access_edit_delete',
+        },
+        {
+            headerName: 'Policy Change',
+            field: 'policy_change',
+        },
+    ];
 
+    featureColumnDefs: ColDef[] = [
+        { headerName: 'Manufacturer URL', field: 'url', pinned: 'left' },
+        {
+            headerName: 'Coherence Score',
+            field: 'coherence_score',
+        },
+        {
+            field: 'entropy',
+        },
+        {
+            headerName: 'Unique Words',
+            field: 'unique_words',
+        },
+        {
+            headerName: 'Reading Time',
+            field: 'reading_time',
+        },
+        {
+            headerName: 'Reading Level',
+            field: 'fkgl',
+        },
+        {
+            headerName: 'Imprecise Words',
+            field: 'imprecise_words',
+        },
+        {
+            headerName: 'Connective Words',
+            field: 'connective_words',
+        },
+        {
+            headerName: 'Correct Grammar',
+            field: 'spelling_errors',
+        },
+    ];
+
+    featureRowData$!: Feature[];
+    keywordRowData$!: Keyword[];
     rowData: ProductData[];
+    featuresData: any[];
+    keywordData: any[];
     defaultColDef: ColDef = {
         sortable: true,
         filter: true,
@@ -100,6 +187,15 @@ export class DataComponent {
                 (f) => f.url === element.manufacturer_url
             );
         });
+    }
+
+    onChange(param: ProductData[]): void {
+        this.selected = param;
+        this.featuresData = this.selected.map((element) => element?.features);
+        this.keywordData = this.selected.map((element) => element?.keywords);
+        this.featureRowData$ = this.featuresData;
+        this.keywordRowData$ = this.keywordData
+        this.showFeatures = this.selected.length > 0;
     }
 
     onKeywordButtonClick(params: ICellRendererParams) {
@@ -128,6 +224,10 @@ export class DataComponent {
 
     onGridReady(params: GridReadyEvent) {
         this.gridApi = params.api;
+        params.api.sizeColumnsToFit();
+    }
+
+    onSecondGridReady(params: GridReadyEvent) {
         params.api.sizeColumnsToFit();
     }
 }
